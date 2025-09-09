ac56ce8eeaa86a5d214e9afbb913a636bdac81eb
Roger Beecham <r.j.beecham@leeds.ac.uk>
2025-09-09 15:24:25 +0100

    updating workplan

--- code/gantt.R
+++ code/gantt.R
@@ -19,18 +19,18 @@ project_data <- data.frame(
     "RB develops MRP code repositories",
     "RB constructs postratification frames +\t
     area-level context variables for FINDS model",
-    "RB plans the RSOS paper",
+    "RB works on the RSOS methods paper",
     
     # Phase 2 tasks
     "RB + SDF data scientists build FINDS MRP model",
     "HASP + SDF data scientist exchange meetings",
-    "RB presents at AAG",
+    "RB presents at AAG 2026",
     
     # Phase 3 tasks
     "SDF data scientists + Project Manager on-board\t
     data products to FINDS catalogue",
-    "RB + HASP data scientists\t
-    write EPB Data:Code paper",
+    "RB + HASP data scientists develop\t
+    and submit EPB Data:Code paper",
     "RB submits methods paper to RSOS",
     
     # Phase 4 tasks
@@ -42,13 +42,13 @@ project_data <- data.frame(
   
   start = as.Date(c(
     # Phase 1 (Oct-Dec 2025)
-    "2025-10-01", "2025-11-01", "2025-11-15",
+    "2025-10-01", "2025-11-01", "2025-11-01",
     
     # Phase 2 (Jan-Mar 2026) 
-    "2026-01-01", "2026-02-01", "2026-03-15",
+    "2025-12-15", "2026-01-15", "2026-03-15",
     
     # Phase 3 (Apr-Jun 2026)
-    "2026-04-01", "2026-05-15", "2026-06-15",
+    "2026-04-01", "2026-05-15", "2026-05-15",
     
     # Phase 4 (Jul-Sep 2026)
     "2026-04-15", "2026-08-01"
@@ -56,13 +56,13 @@ project_data <- data.frame(
   
   end = as.Date(c(
     # Phase 1
-    "2026-04-01", "2025-12-15", "2025-12-31", 
+    "2026-04-01", "2025-12-15", "2026-06-15", 
     
     # Phase 2
     "2026-03-31", "2026-04-15", "2026-03-21",
     
     # Phase 3
-    "2026-06-15", "2026-06-30", "2026-06-30",
+    "2026-06-15", "2026-08-01", "2026-06-15",
     
     # Phase 4
     "2026-09-15", "2026-09-30"
@@ -106,7 +106,11 @@ p <- ggplot(project_data, aes(x = as_date(start), xend = as_date(end),
   
   # Add phase separators
   geom_hline(yintercept = c(2.5, 5.5, 8.5), 
-             linetype = "dashed", color = "gray60", alpha = 0.7) +
+             linetype = "dashed", colour = "#525252", alpha = 0.4, linewidth=.5) +
+  
+  # Add quarter separators
+  geom_vline(xintercept = c(as_date("2025-12-15"), as_date("2026-03-15"), as_date("2026-06-15")), 
+             linewidth=.2, colour = "#525252", alpha = 0.4) +
   
   # Formatting
   scale_x_date(date_breaks = "1 month", 
@@ -121,6 +125,10 @@ p <- ggplot(project_data, aes(x = as_date(start), xend = as_date(end),
 
 p_combined <- ggplot() +
   annotation_custom(as_grob(p), xmin = 0, ymin = 0, xmax = 1, ymax = 1) +
+  
+  annotate("richtext", x=-.06,y=1.11, hjust="left",vjust="top", label.colour = NA, size=6.5, colour="#525252", fill="transparent",
+           label="**Project plan**" ) +
+  
   annotate("richtext", x=-.05,y=.98, hjust="left",vjust="top", label.colour = NA, size=4.5, colour="#525252", fill="transparent",
            label="**Phase 1**<br>Code repositories<br> and postratification <br>frames" ) +
   annotate("richtext", x=-.05,y=.75, hjust="left",vjust="top", label.colour = NA, size=4.5, colour="#525252", fill="transparent",
@@ -130,7 +138,7 @@ p_combined <- ggplot() +
   annotate("richtext", x=-.05,y=.28, hjust="left",vjust="top", label.colour = NA, size=4.5, colour="#525252", fill="transparent",
            label="**Phase 4**<br>Disseminate" ) +
   
-  annotate("segment",  x=0.33,y=0.115, xend=0.33, yend=0.01, colour="#525252", linewidth=.2) +
+  annotate("segment",  x=0.317,y=0.115, xend=0.317, yend=0.01, colour="#525252", linewidth=.2) +
   
   annotate("richtext", x=.2,y=0.05, hjust="middle",vjust="top", label.colour = NA, size=4, colour="#525252", fill="transparent",
            label="2025" ) +
@@ -138,14 +146,100 @@ p_combined <- ggplot() +
   annotate("richtext", x=.6,y=0.05, hjust="middle",vjust="top", label.colour = NA, size=4, colour="#525252", fill="transparent",
            label="2026" ) +
   
-  scale_y_continuous(limits=c(-0.05,1.05), expand=c(0, 0)) +
+  annotation_custom(as_grob(p_person), xmin = -.05, ymin = -.15, xmax = .35, ymax = -.5) +
+  
+  annotate("richtext", x=-.065,y=-.04, hjust="left",vjust="top", label.colour = NA, size=6.5, colour="#525252", fill="transparent",
+           label="**Personnel allocation**" ) +
+  
+  annotate("richtext", x=.048,y=-.12, hjust="middle",vjust="top", label.colour = NA, size=4, colour="#525252", fill="transparent",
+           label="HASP" ) +
+  
+  annotate("richtext", x=.2,y=-.12, hjust="middle",vjust="top", label.colour = NA, size=4, colour="#525252", fill="transparent",
+           label="SDF" ) +
+  
+  annotate("richtext", x=.37,y=-.38, hjust="left",vjust="top", label.colour = NA, size=3.3, colour="#525252", fill="transparent",
+           label="*3xDS column*<br>HASP Data Scientists are<br>not directly costed." ) +
+  
+  annotate("richtext", x=.37,y=-.18, hjust="left",vjust="top", label.colour = NA, size=3.3, colour="#525252", fill="transparent",
+           label="Circles are sized proportionally<br>to time-adjusted workload. <br> So RB is working 22% over<br>12 months, SDF data scientists<br> 40% in 9 months." ) +
+  
+  
+ # annotate("curve", x=.34, xend=.09, y=-.43, yend=-.43, linewidth=.2, colour="#969696", arrow = arrow(length = unit(0.006, "npc")), curvature = -0.3) + 
+  
+  annotate("segment",  x=0.0895,y=-0.14, xend=0.0895, yend=-0.22, colour="#969696", linewidth=.15) +
+  
+  scale_y_continuous(limits=c(-0.5,1.11), expand=c(0, 0)) +
   scale_x_continuous(limits=c(-.1,1.05), expand=c(0, 0)) +
   theme(
     axis.text=element_blank(), 
     axis.line = element_blank(), 
     axis.title.x = element_blank(), axis.title.y = element_blank())
 
-quartz(file = here("figs", "workplan.png"), type = "png", dpi = 300, width = 11, height = 6)
+quartz(file = here("figs", "workplan.png"), type = "png", dpi = 300, width = 11, height = 8.5)
 print(p_combined)
 dev.off()
+
+
+person_data <- data.frame(
+  phase = rep(c("P1","P2", "P3", "P4"), times=7, each=1),
+  person = rep(c("RB", 
+                   "3xDS", 
+                   "DS", 
+                   "SDS", 
+                   "CDS", 
+                   "PM", 
+                   "DC&S"),
+                   times=1, each=4),
+  time=c(
+    22,22,22,22,
+    0, 10, 10, 10,
+    0, 40, 40, 40,
+    0, 40, 40, 40,
+    0, 5, 5, 5,
+    0, 0, 3, 10,
+    0, 0, 0, 13
+    )
+  ) |> 
+  mutate(person=factor(person, levels=c("RB", 
+                       "3xDS", 
+                       "DS", 
+                       "SDS", 
+                       "CDS", 
+                       "PM", 
+                       "DC&S")),
+         phase=factor(phase, levels=c("P1","P2", "P3", "P4")))
+
+p_person <- person_data |> 
+  mutate(phase=fct_rev(phase)) |> 
+  ggplot(aes(y=phase,x=person)) +
+  geom_tile(colour="#969696", fill="transparent", linewidth=.1) +
+  #geom_tile(data = . %>% filter(person!="3xDS"), colour="#f0f0f0", fill="transparent", linewidth=.5) +
+  #geom_tile(data = . %>% filter(person=="3xDS"), colour="#f0f0f0", fill="#f0f0f0", linewidth=.5) +
+  geom_point(data = . %>% filter(time>0, person!="3xDS"), aes(size=time, fill=phase, colour=phase), alpha=1, pch=21) +
+  geom_point(data = . %>% filter(time>0, person=="3xDS"), 
+             aes(size=time, colour=phase, fill=phase), alpha=.5, pch=21) +
+  scale_colour_manual(values=c("#fb9a99", "#b2df8a", "#fdbf6f", "#a6cee3")) +
+  scale_fill_manual(values=c("#fb9a99", "#b2df8a", "#fdbf6f", "#a6cee3")) +
+  scale_x_discrete(position = "top") +
+  guides(fill="none", colour="none", size="none") +
+  theme(
+    axis.title.x = element_blank(), axis.title.y = element_blank(),
+    axis.line = element_blank(),
+    axis.text.y = element_text(face="bold")
+  )
+  
+  
+  time = c()
+  )
+                 
+                 
+                 
+             rep("3xDS\nHASP", times=4, each=1), 
+             rep("DS\nSDF", times=4, each=1), 
+             rep("SDS\nSDF", times=4, each=1),
+             rep("CDS\nSDF",times=4, each=1), 
+             rep("PM\nSDF",times=4, each=1), 
+             rep("DC&S\nSDF",times=4, each=1))
+  ) 
+
 
